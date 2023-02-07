7b3e737f567b7f433ba3c9079be50d968a7169f5
amrbashir <amr.bashir2015@gmail.com>
2023-02-07 17:55:07 +0200

    fix: match loading icon size to maginifer icon

---
+++ .changes/loading-icon-size.md
@@ -0,0 +1,5 @@
+---
+"meilisearch-docsearch": "patch"
+---
+
+Change loading icon size to match the magnifier icon size inside the search input.
--- src/icons/Loading.tsx
+++ src/icons/Loading.tsx
@@ -4,8 +4,8 @@ export const LoadingIcon: Component<{ class?: string }> = (props) => {
   return (
     <svg
       class={props.class}
-      width="20"
-      height="20"
+      width="24"
+      height="24"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path fill="currentColor" d="M12 4V2A10 10 0 0 0 2 12h2a8 8 0 0 1 8-8Z" />
