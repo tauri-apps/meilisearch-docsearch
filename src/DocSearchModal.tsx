b6e0e24476d4beb8b7bd07aeb357b22254d645bc
amrbashir <github@amrbashir.me>
2025-03-24 15:04:51 +0200

    revert unrelevant changes

--- src/DocSearchModal.tsx
+++ src/DocSearchModal.tsx
@@ -317,10 +317,6 @@ export const DocSearchModal: Component<DocSearchModalProps> = ({
                             <a
                               href={hit.url || "#"}
                               aria-label={linkToTheResultAriaLabel}
-                              onClick={(e) => {
-                                // e.preventDefault();
-                                onClose && onClose();
-                              }}
                             >
                               <span class="docsearch-modal-search-hits-item-text-container">
                                 <p
